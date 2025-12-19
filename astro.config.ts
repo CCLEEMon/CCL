0a713ab767fd1eefe84a548b269c8f658d13b9e9
CCLEEMon <leecc1531@gmail.com>
2025-12-19 14:42:36 +0800

    add

--- astro.config.ts
+++ astro.config.ts
@@ -4,6 +4,8 @@ import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://ccleemon.github.io',
+  base: '/CCL',
   integrations: [icon()],
   server: {
     host: '0.0.0.0',
