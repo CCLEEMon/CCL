1c13ec3fa29becf38669e98ace915fc6e51aedb2
CCLEEMon <leecc1531@gmail.com>
2025-12-19 14:53:15 +0800

    1

--- astro.config.ts
+++ astro.config.ts
@@ -5,6 +5,7 @@ import icon from 'astro-icon';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://ccl.aigent.ren',
+  base: '/',
   integrations: [icon()],
   server: {
     host: '0.0.0.0',
